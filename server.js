4bcc6e4488ff059ac5345bd2f83b4e89fe1a5e27
KanKan <cindykan@KanKan.local>
2018-01-29 17:09:06 -1000

    getHandler function

--- server.js
+++ server.js
@@ -4,52 +4,29 @@ const fs = require('fs');
 const querystring = require('querystring');
 const PORT = 8080;
 
-
-
-
-
-// console.log(hydrogen);
-// debugger
-
-// const css = require('./css/styles.css');
-// const {
-//   _404,
-//   index,
-//   helium,
-//   hydrogen
-// } = require('./pages');
-
-
 const server = http.createServer((req, res) => {
   let url = req.url;
-    if (url === '/'){
-      url += 'index.html';
-    }
-    url = 'public' + url;
-    console.log(url);
+  if (url === '/') {
+    url += 'index.html';
+  }
+  url = 'public' + url;
+  console.log(url);
 
-  // case  for GET
-  if(req.method === `GET`) {
-    
-    
-  
-  
-  
-  
-} else if (req.method === 'POST') { //case for POST
-  req.on('data', (data) => {
-    let elementChunk = querystring.parse(data.toString());
-    console.log(elementChunk);
-    
-  });
-  
+  // case for GET
+  if (req.method === `GET`) {
+    getHandler(url, res)
+
+  } else if (req.method === 'POST') { //case for POST
+    req.on('data', (data) => {
+      let elementChunk = querystring.parse(data.toString());
+      console.log(elementChunk);
+
+    });
+
   } else { //case for neither
     res.writeHead(405, 'Unsupported method')
     res.end()
-  } 
-  
-  
-
+  }
 
 
 }); //closing for server.on
@@ -57,3 +34,18 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log('Server listening on ' + PORT);
 }); //closing for listen
+
+function getHandler(url, res) {
+  fs.readFile(url, (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+    res.writeHead()
+    res.write(data.toString());
+    res.end();
+
+  })
+}
+
+// function setSuccessHeader(res) {
+//   res.se
+// }
